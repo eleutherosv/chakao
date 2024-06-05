d1b5da8e1e3328f10b7f3e86f11329eedcfe2cb6
DudeWeb <95248247+Dudeweb@users.noreply.github.com>
2024-06-04 19:18:18 -0500

    2nd slide section light

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,6 @@ module.exports = {
         pacifico: ["Pacifico", "system-ui"]
       }
     }
-      colors: {
-      chakao: {
-        dark: '#2e2438',  // Darker variant
-        light: '#5c4a75',  // Lighter variant
-        base: '#433253',  // Base color
-        medium: '#4c3a62',  // Medium variant
-        bckg: '#e4e2eb',  // Background variant
-      },
-    },
+     
   }
 }
